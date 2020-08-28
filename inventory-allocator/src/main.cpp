a7f7912614f1a4491fedccc5c418ac24306b9b1e
Jacob Brolliar <jabrolliar@mail.csuchico.edu>
2020-08-27 21:42:08 -0700

    Input parsing

--- inventory-allocator/src/main.cpp
+++ inventory-allocator/src/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+//#include <fstream>
 #include <string>
+#include <sstream>
 #include <vector>
 #include <map>
 
@@ -9,7 +11,10 @@ map<string, int> stock(string);
 
 
 int main(int argc, char *argv[]) {
-  string input = "";
+  //ifstream order(argv[1]);
+  //ifstream warehouses(argv[2]);
+  string input = "house apple 1 orange 2 corn 5";
+  int num = 0;
   //take user's order
 
   //take warehouse information reading first line then storing map into vector
@@ -20,12 +25,27 @@ int main(int argc, char *argv[]) {
   vector<map<string, int>> warehouse;
   int space = input.find(" ");
   name.push_back(input.substr(0, space));
-  string list = input.substr(space);
-  //cout << name << endl << list <<endl;
-  //warehouse.push_back(stock(list));
+  string list = input.substr(space+1);
+  warehouse.push_back(stock(list));
+
+  cout << warehouse[0]["apple"] << endl;
+  cout << warehouse[0]["bannana"] << endl;
 
   //if no single warehouse can complete order
   //then use vector of maps to piece together order
 
   return 0;
+}
+
+map<string, int> stock(string line){
+  map<string, int> inv;
+  string temp;
+  stringstream ss;
+  string count;
+  ss << line;
+  while(ss >> temp){
+      ss >> count;
+      inv[temp] = stoi(count);
+  }
+  return inv;
 }
